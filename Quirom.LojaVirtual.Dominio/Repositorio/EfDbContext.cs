1706197ab7672ef73f6164f5531df234b885209a
ruthnaldosantos <ruthnaldo.santos@outlook.com>
2015-02-11 18:04:31 -0300

    Aula

---
+++ Quirom.LojaVirtual.Dominio/Repositorio/EfDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Quirom.LojaVirtual.Dominio.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quirom.LojaVirtual.Dominio.Repositorio
+{
+    public class EfDbContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+
+    }
+}
